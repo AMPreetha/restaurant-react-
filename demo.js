8224c75962fff8ff2abd2f4d7b30697d828a119c
AMPreetha <127812727+AMPreetha@users.noreply.github.com>
2023-03-14 11:09:23 +0530

    Update demo.js

--- demo.js
+++ demo.js
@@ -1,1 +1,88 @@
-console.log("hello")
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Registration page</title>
+</head>
+<body>
+    <style ="text/css">
+        h1.h1_blue{
+            color:navy;
+        }
+        h.h_blue{
+            color:brown
+        }
+    </style>
+    <div class="row">
+    <div style="background-color: rgba(128, 218, 245, 0.37); border-color: blue; align-content: space-around;">
+    <center> <h1 class='h1_blue' style="background-color: rgba(196, 202, 255, 0.336); border-color: rgb(27, 27, 206);"> PROJECT REGISTRATION</h1></center>
+    <center>
+        <forms>
+            <h class="h_blue" style= "background-color:rgb(155, 250, 167)";"><b> REGISTER NOW!</b></h>
+            <br>
+            <br>
+            <b>NAME:</b>
+            <input type="text" name="username" required><br>
+            <br>
+            <b>PASSWORD:</b>
+            <input type="password" name="user_pass" required><br>
+            <br>
+            <b>GENDER</b>:
+            <input type="radio" name="r_gender">Male
+            <input type="radio" name="r_gender">Female
+            <input type="radio" name="r_gender">Other
+            <br>
+            <br>
+            <b>MARITAL STATUS:</b>
+            <input type="checkbox" name="c_gender" >Married
+            <input type="checkbox" name="c_gender">Unmarried
+            <br>
+            <br>
+            <b> DEGREE:</b>
+            <select name="s_box">
+                <option value="s_B.E">B.E</option>
+                <option value="s_B.E">B.A</option>
+                <option value="s_B.E">B.SC</option>
+            </select>
+            <br>
+            <br>
+            <b>FILE INPUT: </b>
+            <button>Browse...</button>No selected files
+            <br>
+            <p style="font-style:italic; color:rgba(0, 0, 0, 0.521);">Example resume,educational qualification</p>
+            <br>
+            <button><h>submit</button></h>
+            <a href="Forgot password" style="color:skyblue">Forgot password</a></p>
+            
+        </forms>
+    </center>
+</div>
+</div>
+<h id="title">hello</h>
+    <style ='text/css'>
+        p.p_color{
+            color:blue;
+            font-weight:bold;
+        }
+    </style>
+    <p class='p_color' >please be informed that this session is recoded for quality purposes</p>
+    <ol><b>FILE INPUT KEY:</b>
+        <br>
+        <br>
+        <li><i>files should be in the format of pdf only.</i></li>
+        <br>
+        <li><i>insert the pdf under 2mb.</i></li><span>
+        
+    
+    </span>
+    <script type="text/javascript">
+        document.write("hello<br>")
+    </script>
+    <script src="demo">
+        
+        </script>
+</body>
+</html>
